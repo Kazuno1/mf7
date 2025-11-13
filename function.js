8d6f148b92bee92cc8537c1411687948176a780b
Kazuno1 <152275365+Kazuno1@users.noreply.github.com>
2025-11-13 22:26:48 +0800

    Add files via upload

--- function.js
+++ function.js
@@ -1,14 +1,12 @@
 // mobile menu
-document.addEventListener("DOMContentLoaded", () => {
-  const mobileToggle = document.getElementById("mobileToggle");
-  const navLinks = document.getElementById("navLinks");
+const mobileToggle = document.getElementById('mobileToggle');
+const navLinks = document.getElementById('navLinks');
 
-  if (mobileToggle && navLinks) {
-    mobileToggle.addEventListener("click", () => {
-      navLinks.classList.toggle("open");
-    });
-  }
-});
+if (mobileToggle && navLinks) {
+  mobileToggle.addEventListener('click', () => {
+    navLinks.classList.toggle('open');
+  });
+}
 
 // cookie close
 const cookieClose = document.getElementById('cookieClose');
